f7a01e1c45669535cf7ea85c5aad96ee90d1d872
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-05-29 14:08:02 +0200

    First simple version api with weather in Austin.

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
+$(function () {
 
-    
+
+
+
+    $(document).ready(function () {
+        $.simpleWeather({
+            location: 'Austin, TX',
+            woeid: '',
+            unit: 'f',
+            success: function (weather) {
+                html = '<h2 class="temp"><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2><h2 class="temp" style="display: none">' + weather.alt.temp + '&deg;' + weather.alt.unit + '</h2>';
+                html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
+                html += '<li class="currently">' + weather.currently + '</li>';
+                html += '<li>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
+
+                $("#weather").html(html);
+                const $temp = $('.temp');
+                $temp.click(function () {
+                    $temp.slideToggle('fast');
+                });
+            },
+            error: function (error) {
+                $("#weather").html('<p>' + error + '</p>');
+            }
+        });
+
+    });
+
+
+
 });
